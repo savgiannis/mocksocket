06c454349affeb2df3d070559643db8e21eaf521
monkeydkon <kon_fu2@hotmail.com>
2019-12-09 14:46:05 +0200

    heroku

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const io = require('socket.io')(server);
 
 let mySocket;
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.get('/taResponse', function (req, res) {
 
   if (mySocket) {
